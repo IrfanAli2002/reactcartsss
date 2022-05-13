c3aa3a4f0dc27feecaf7452fde3ead2580e6f2aa
irfanAli <daniyaliffi123@gmail.com>
2022-05-13 06:07:42 -0700

    complte

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import {usestate} from 'react'
+import {Arry} from './components/Arry'
+
 
 function App() {
-  return (
+ const [arry,setarry]= usestate(
+
+   [
+     {
+       title:"Moto Z",
+       price:"20,000 PKR",
+       quantity:"5 left",
+       description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, vitae."
+      }
+    ]
+    );
+//     const [secondarry,setsecondarry]= usestate[
+//    {
+//     title:"Moto Z2force",
+//     price:"30,000 PKR",
+//     quantity:"11 left",
+//     description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, vitae."
+//    }
+//  ]
+//  const [thirdarry,setthirdarry]= usestate[
+//    {
+//     title:"iphone X",
+//     price:"90,000 PKR",
+//     quantity:"8 left",
+//     description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, vitae."
+//    }
+//  ]
+//  const [fortharry,setfortharry]= usestate[
+//    {
+//     title:"iphone 8plus",
+//     price:"55,000 PKR",
+//     quantity:"16 left",
+//     description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, vitae."
+//    }
+//  ]
+
+
+ let addtoCart = ()=>{
+alert(`"titleiphone 8plus",
+  "price55,000 PKR",
+  "quantity16 left",
+  "descriptionLorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, vitae."`)
+ }
+  
+ 
+
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {arry.map((e) => (
+        <Arry title={e.title} price={e.price} Quantity={e.Quantity}  Description={e.Description}
+        action={addtoCart}  />
+
+      ))}
+
+
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Arry.js
@@ -0,0 +1,12 @@
+function Arry(props){
+    return(
+
+        <div>
+        {/* <h1>{props.title}</h1>
+        <h1>{props.price}</h1> */}
+        <img src="https://pakistanprice.pk/wp-content/uploads/2021/01/Motorola-Moto-Z-Play-White-Fine-Color.jpeg" />
+        <button onClick={props.acion}>addtoCart</button>
+    </div>
+        )
+}
+export {Arry}
